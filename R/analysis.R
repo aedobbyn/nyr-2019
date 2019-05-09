4ace5444ac95fb9ef4e381b68067b2e87c1b96a6
aedobbyn <adobbs11@gmail.com>
2019-05-08 22:33:15 -0400

    figure out average sunrise and sunset times for all year

--- R/analysis.R
+++ R/analysis.R
@@ -144,12 +144,12 @@ by_hour %>%
       pull(hour_label)
   ) +
   geom_smooth(se = FALSE, linetype = "dashed", size = 0.5, method = "loess") +
-  geom_vline(xintercept = 5.667, linetype = "dotted") +
-  annotate("text", x = 6.667, y = 120, 
-           label = "Sunrise", family = "Arial", size = 2) +
-  geom_vline(xintercept = 20, linetype = "dotted") +
-  annotate("text", x = 21, y = 160, 
-           label = "Sunset", family = "Arial", size = 2) +
+  geom_vline(xintercept = 6.5, linetype = "dotted") +
+  annotate("text", x = 7.5, y = 120, 
+           label = "Average \n Sunrise", family = "Arial", size = 2) +
+  geom_vline(xintercept = 18.667, linetype = "dotted") +
+  annotate("text", x = 19.667, y = 160, 
+           label = "Average \n Sunset", family = "Arial", size = 2) +
   labs(x = "Time of Day", y = "N Fires") +
   ggtitle("Fires by Hour") +
   theme_minimal() +
@@ -167,3 +167,117 @@ write_csv(fires_by_borough,
           here("data", "derived", "fires_by_borough.csv"))
 write_csv(fires_by_borough_per_cap, 
           here("data", "derived", "fires_by_borough_per_cap.csv"))
+
+
+
+
+
+# Get average sunrise and sunset times
+
+clean_time <- function(x) {
+  x %>% 
+    str_extract_all(".+[am|pm]") %>% 
+    as_vector() 
+}
+
+get_mins <- function(df, col = sunset) {
+  q_col <- enquo(col)
+  
+  df %>% 
+    separate(
+    !!q_col, c("hr", "mn", "am_pm"),
+    sep = "[ :]",
+    remove = FALSE
+  ) %>% 
+    mutate(
+      hr = as.numeric(hr),
+      hr = 
+        case_when(
+          am_pm == "pm" ~ hr + 12,
+          TRUE ~ hr
+        ),
+      mn = as.numeric(mn),
+      total = (hr*60) + mn
+    ) %>% 
+    select(-am_pm) %>% 
+    drop_na()
+}
+  
+get_sun_rise_set <- function(month = 1,
+                             base_url = 
+                              "https://www.timeanddate.com/sun/usa/new-york?month=",
+                             year = 2018,
+                             return = "sunrise") {
+  
+  
+  url <- glue("https://www.timeanddate.com/sun/usa/new-york?month={month}&year={year}") 
+  
+  df <- url %>% 
+    xml2::read_html() %>%
+    rvest::html_nodes("table") %>%
+    rvest::html_table(fill = TRUE, header = FALSE) %>% 
+    purrr::pluck(1) %>% 
+    as_tibble(.name_repair = "unique") %>% 
+    select(1:3) 
+  
+  names(df) <- c("day", "sunrise", "sunset")
+  
+  df <- df %>% 
+    slice(4:nrow(.)) %>% 
+    mutate(
+      sunrise = 
+        sunrise %>% 
+        map_chr(clean_time),
+      sunset = 
+        sunset %>% 
+        map_chr(clean_time),
+      month = month
+    ) %>% 
+    select(month, day, everything()) 
+  
+  sunsets <- 
+    df %>% 
+    select(-sunrise)
+
+  sunsets <- 
+    sunsets %>% 
+    get_mins(col = sunset)
+  
+  sunrises <- 
+    df %>% 
+    select(-sunset) %>% 
+    get_mins(col = sunrise)
+  
+  if (return == "sunrise") {
+    sunrises
+  } else if (return == "sunset") {
+    sunsets
+  }
+}
+
+
+all_sunrises <- 
+  map_df(1:12, .f = get_sun_rise_set, return = "sunrise")
+
+
+all_sunsets <- 
+  map_df(1:12, .f = get_sun_rise_set, return = "sunset")
+
+
+summarise_sun_rise_set <- function(tbl) {
+  tbl %>% 
+    summarise(
+      mean_mins = mean(total, na.rm = TRUE)
+    ) %>% 
+    mutate(
+      hr = (mean_mins/60) %>% floor(), 
+      mn = mean_mins - (hr*60)
+    )
+}
+
+all_sunrises %>% summarise_sun_rise_set()
+all_sunsets %>% summarise_sun_rise_set()
+  
+
+  
+
